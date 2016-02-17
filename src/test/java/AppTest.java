2b1655d0bf92a693f982e4c0293cb4566078ca86
Brad London and Charles Carter <student@epicodus.com>
2016-02-17 11:43:40 -0800

    add spark routes for front end functionality

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,5 +1,5 @@
 import java.util.ArrayList;
-
+import org.junit.Rule;
 import org.junit.*;
 import static org.junit.Assert.*;
 import java.util.ArrayList;
@@ -13,9 +13,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class AppTest extends FluentTest {
   public WebDriver webDriver = new HtmlUnitDriver();
+
+  @Override
   public WebDriver getDefaultDriver() {
       return webDriver;
   }
+
+  @Rule
+  public ClearRule clearRule = new ClearRule();
 
   @ClassRule
   public static ServerRule server = new ServerRule();
@@ -23,12 +28,13 @@ public class AppTest extends FluentTest {
   @Test
   public void rootTest() {
       goTo("http://localhost:4567/");
-      assertThat(pageSource()).contains("Task list!");
+      assertThat(pageSource()).contains("Todo list!");
   }
 
   @Test
   public void taskIsCreatedTest() {
       goTo("http://localhost:4567/");
+      click("a", withText("Add a new task"));
       fill("#description").with("Mow the lawn");
       submit(".btn");
       assertThat(pageSource()).contains("Your task has been saved.");
@@ -36,22 +42,22 @@ public class AppTest extends FluentTest {
 
   @Test
   public void taskIsDisplayedTest() {
-      goTo("http://localhost:4567/");
+      goTo("http://localhost:4567/tasks/new");
       fill("#description").with("Mow the lawn");
       submit(".btn");
-      click("a", withText("Go Back"));
+      click("a", withText("View All Tasks"));
       assertThat(pageSource()).contains("Mow the lawn");
   }
 
   @Test
   public void multipleTasksAreDisplayedTest() {
-    goTo("http://localhost:4567/");
+    goTo("http://localhost:4567/tasks/new");
     fill("#description").with("Mow the lawn");
     submit(".btn");
-    click("a", withText("Go Back"));
+    goTo("http://localhost:4567/tasks/new");
     fill("#description").with("Buy groceries");
     submit(".btn");
-    click("a", withText("Go Back"));
+    click("a", withText("View All Tasks"));
     assertThat(pageSource()).contains("Mow the lawn");
     assertThat(pageSource()).contains("Buy groceries");
   }
